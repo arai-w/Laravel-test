a95d9142c7ae44f2d9742bb715ffd29f4d6e2fb7
arai-w <arai-w@hi-ad.jp>
2020-04-08 17:49:51 +0900

    2

--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -14,6 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('hello',function(){
-    return '<html><body><h1>Hello</h1><p>This is sample page</p></body></html>';
+// Route::get('hello',function(){
+//     return '<html><body><h1>Hello</h1><p>This is sample page</p></body></html>';
+// });
+Route::get('hello1/{msg}',function($msg)
+{
+    $html = <<< EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16pt;color:#999;}
+</style>
+
+<body>
+    <h1> Hello </h1>
+    <p>{$msg}</p>
+</body>
+
+</head>
+</html>
+EOF;
+
+    return $html;
 });
